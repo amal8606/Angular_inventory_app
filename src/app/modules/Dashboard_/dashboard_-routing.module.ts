553d6c3414a47fa26fc612f396bc632c5333f23b
Amal Abraham <amal.abraham@wenmax.com>
2023-03-24 14:11:05 +0530

    new

--- src/app/modules/Dashboard_/dashboard_-routing.module.ts
+++ src/app/modules/Dashboard_/dashboard_-routing.module.ts
@@ -5,7 +5,8 @@ import { Dashboard_Component } from './dashboard_.component';
 
 const routes: Routes = [{ path: '', component: Dashboard_Component ,canActivate:[dashBoardGuard],
 children:[{ path: 'products', loadChildren: () => import('../../modules/dashboard/dashboard/dashboard.module').then(m => m.DashboardModule) },
-{ path: 'clients', loadChildren: () => import('../../modules/clients/clients.module').then(m => m.ClientsModule)}
+{ path: 'clients', loadChildren: () => import('../../modules/clients/clients.module').then(m => m.ClientsModule)},
+{path:'sales',loadChildren:()=>import('../sales/sales.module').then(m=>m.SalesModule)}
 ]
 }];
 
